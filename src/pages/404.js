80cda2e55371f344e91d34b24fdeb0386a737008
Kenn Stack <kibadikennedy@gmail.com>
2022-03-10 19:48:50 +0800

    hahah

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,37 @@
 import React from "react"
-
+import { Link } from "gatsby"
+import Button from "../../src/components/Buttons/Button"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import NotFoundSVG from "../../assets/notfound.svg"
 
-const NotFoundPage = () => (
+const NotFound = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO
+      title="NotFound | WebContract"
+      description="Freelance Website Web Developer Headless CMS JAMStack technologies"
+    />
+
+    <div
+      id="home"
+      className="flex flex-col lg:flex-row justify-between mx-auto my-5 py-6 lg:py-6 md:-mt-16 lg:-mt-0"
+    >
+      <NotFoundSVG className="w-2/3 mx-auto md:w-2/3 lg:w-2/4 my-auto" />
+      <div className="w-full md:w-1/3 lg:w-2/4 flex mx-auto my-auto flex-col md:space-y-6 justify-between">
+        <h1 className="text-center text-5xl md:text-5xl lg:text-6xl font-extrabold">
+          Oops!
+        </h1>
+        <p className="text-center text-turbo-gray-900 text-lg font-medium my-10">
+          You're lost!
+        </p>
+        <div className="mx-auto">
+          <Link to="/">
+            <Button content={"See Home Page!"} />
+          </Link>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
-export default NotFoundPage
+export default NotFound
