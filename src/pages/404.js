3308dfd6fb348448fc92f2254d6cd4813b8267d8
Kenn Stack <kibadikennedy@gmail.com>
2022-03-11 19:16:06 +0800

    404 page bug persists

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,33 +5,39 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import NotFoundSVG from "../../assets/notfound.svg"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO
-      title="NotFound"
-      description="Freelance Website Web Developer Headless CMS JAMStack technologies"
-    />
+const browser = typeof window !== "undefined" && window
 
-    <div
-      // id="home"
-      className="flex flex-col lg:flex-row justify-between mx-auto my-5 py-6 lg:py-6 md:-mt-16 lg:-mt-0"
-    >
-      <NotFoundSVG className="w-2/3 mx-auto md:w-2/3 lg:w-2/4 my-auto" />
-      <div className="w-full md:w-1/3 lg:w-2/4 flex mx-auto my-auto flex-col md:space-y-6 justify-between">
-        <h1 className="text-center text-5xl md:text-5xl lg:text-6xl font-extrabold">
-          Oops!
-        </h1>
-        <p className="text-center text-turbo-gray-900 text-lg font-medium my-10">
-          You're lost!
-        </p>
-        <div className="mx-auto">
-          <Link to="/">
-            <Button content={"See Home Page!"} />
-          </Link>
+const NotFoundPage = () => {
+  return (
+    browser && (
+      <Layout>
+        <SEO
+          title="NotFound"
+          description="Freelance Website Web Developer Headless CMS JAMStack technologies"
+        />
+
+        <div
+          // id="home"
+          className="flex flex-col lg:flex-row justify-between mx-auto my-5 py-6 lg:py-6 md:-mt-16 lg:-mt-0"
+        >
+          <NotFoundSVG className="w-2/3 mx-auto md:w-2/3 lg:w-2/4 my-auto" />
+          <div className="w-full md:w-1/3 lg:w-2/4 flex mx-auto my-auto flex-col md:space-y-6 justify-between">
+            <h1 className="text-center text-5xl md:text-5xl lg:text-6xl font-extrabold">
+              Oops!
+            </h1>
+            <p className="text-center text-turbo-gray-900 text-lg font-medium my-10">
+              You're lost!
+            </p>
+            <div className="mx-auto">
+              <Link to="/">
+                <Button content={"See Home Page!"} />
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </Layout>
-)
+      </Layout>
+    )
+  )
+}
 
 export default NotFoundPage
